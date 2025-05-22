3a0c3520b3e931725f2c3969f320c4ab89e1ac3e
aaa4589 <anwer7149@gmail.com>
2025-05-22 16:58:11 +0100

    ajout de classe joueur

---
+++ src/main/java/anwer/TP2/exo4/Joueur.java
@@ -0,0 +1,7 @@
+package anwer.TP2.exo4;
+
+public interface Joueur {
+    public int mise(); // on suppose que mise positive
+    public void debiter(int somme) throws DebitImpossibleException;
+    public void crediter(int somme);
+} 
