6ddc22e34b8488205d4fd92d006494dcd97cab33
aaa4589 <anwer7149@gmail.com>
2025-05-22 16:23:04 +0100

    ajout de classe utilisateur  (nom , prenom, email )

---
+++ src/main/java/anwer/TP2/exo2/Utilisateur.java
@@ -0,0 +1,18 @@
+package anwer.TP2.exo2;
+
+public class Utilisateur {
+    private String nom;
+    private String prenom;
+    private String email;
+
+    public Utilisateur(String nom, String prenom, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
+    // Getters pour les tests
+    public String getNom() { return nom; }
+    public String getPrenom() { return prenom; }
+    public String getEmail() { return email; }
+}
